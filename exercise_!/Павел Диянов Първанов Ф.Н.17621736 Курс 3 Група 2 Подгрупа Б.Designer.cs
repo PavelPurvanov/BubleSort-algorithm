8c233270dcddd442960917c5cdc420d1234d0587
PavelPurvanov <61511169+PavelPurvanov@users.noreply.github.com>
2020-09-28 17:57:57 +0300

    new

---
+++ exercise_!/Павел Диянов Първанов Ф.Н.17621736 Курс 3 Група 2 Подгрупа Б.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace exercise__
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1_scan = new System.Windows.Forms.Button();
+            this.button2_sort = new System.Windows.Forms.Button();
+            this.button3_result = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Numbers = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Counting = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(381, 289);
+            this.label1.TabIndex = 0;
+            // 
+            // button1_scan
+            // 
+            this.button1_scan.Location = new System.Drawing.Point(12, 365);
+            this.button1_scan.Name = "button1_scan";
+            this.button1_scan.Size = new System.Drawing.Size(114, 23);
+            this.button1_scan.TabIndex = 1;
+            this.button1_scan.Text = "1. Scanner";
+            this.button1_scan.UseVisualStyleBackColor = true;
+            this.button1_scan.Click += new System.EventHandler(this.button1_scan_Click);
+            // 
+            // button2_sort
+            // 
+            this.button2_sort.Location = new System.Drawing.Point(146, 365);
+            this.button2_sort.Name = "button2_sort";
+            this.button2_sort.Size = new System.Drawing.Size(114, 23);
+            this.button2_sort.TabIndex = 2;
+            this.button2_sort.Text = "2. Sort";
+            this.button2_sort.UseVisualStyleBackColor = true;
+            this.button2_sort.Click += new System.EventHandler(this.button2_sort_Click);
+            // 
+            // button3_result
+            // 
+            this.button3_result.Location = new System.Drawing.Point(279, 365);
+            this.button3_result.Name = "button3_result";
+            this.button3_result.Size = new System.Drawing.Size(114, 23);
+            this.button3_result.TabIndex = 3;
+            this.button3_result.Text = "3. Result";
+            this.button3_result.UseVisualStyleBackColor = true;
+            this.button3_result.Click += new System.EventHandler(this.button3_result_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 339);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(114, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(146, 339);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(114, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(279, 339);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(114, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "0";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(12, 308);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 28);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Време за сканиране";
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(146, 308);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 28);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Време за сортиране";
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(279, 308);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 28);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Време за преброяване";
+            // 
+            // textBox4
+            // 
+            this.textBox4.AcceptsReturn = true;
+            this.textBox4.Location = new System.Drawing.Point(418, 38);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(115, 321);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(418, 369);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Брой числа";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(491, 369);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(42, 20);
+            this.textBox5.TabIndex = 12;
+            this.textBox5.Text = "0";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(561, 38);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox6.Size = new System.Drawing.Size(115, 351);
+            this.textBox6.TabIndex = 13;
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Numbers,
+            this.Counting});
+            this.dataGridView1.Location = new System.Drawing.Point(694, 9);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.Size = new System.Drawing.Size(193, 380);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // Numbers
+            // 
+            this.Numbers.HeaderText = "ЧИСЛА";
+            this.Numbers.Name = "Numbers";
+            this.Numbers.ReadOnly = true;
+            // 
+            // Counting
+            // 
+            this.Counting.HeaderText = "БРОЙ";
+            this.Counting.Name = "Counting";
+            this.Counting.ReadOnly = true;
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(418, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 26);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "ЧИСЛА";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Location = new System.Drawing.Point(561, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(115, 26);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "СОРТИРАНИ";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(891, 395);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3_result);
+            this.Controls.Add(this.button2_sort);
+            this.Controls.Add(this.button1_scan);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Павел Диянов Първанов Ф.Н.17621736 Курс 3 Група 2 Подгрупа Б";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1_scan;
+        private System.Windows.Forms.Button button2_sort;
+        private System.Windows.Forms.Button button3_result;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Numbers;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Counting;
+    }
+}
+
---
+++ exercise_!/Павел Диянов Първанов Ф.Н.17621736 Курс 3 Група 2 Подгрупа Б.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace exercise__
+{
+    public partial class Form1 : Form
+    {
+        string myString;
+        List<int> myList;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (var streamReader = new StreamReader(@"C:\Users\boysa\Documents\Visual Studio 2012\Projects\exercise_1_17621736_SIT\stringFile.txt", Encoding.UTF8))
+            {
+                myString = streamReader.ReadToEnd();
+            }
+
+            label1.Text = myString;
+        }
+
+        public void generate()
+        {
+            int counter = 0;
+            Stopwatch MyTimer = new Stopwatch();
+
+            MyTimer.Start();
+
+            myList = myString.Split(' ').Select(Int32.Parse).ToList();
+
+           foreach (int var in myList)
+            {
+                counter++;
+                textBox4.Text += System.Environment.NewLine + var.ToString();
+            }
+
+            textBox1.Text = MyTimer.Elapsed.ToString();
+            textBox5.Text = counter.ToString();
+            MyTimer.Stop();
+
+            int firstWord = myString.Split(' ').Select(Int32.Parse).First();
+            int lastWord = myString.Split(' ').Select(Int32.Parse).Last();
+            if (myList.First() == firstWord && myList.Last() == lastWord)
+            {
+                MessageBox.Show("Първото и последно число се съдържат в колекцията !" + " Първо число: " + myList.First() + " Последно число: " + myList.Last(),"MsgBox" , MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            else
+            {
+                MessageBox.Show("Първото и последно число не се съдържат в колекцията !", "MsgBox", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+
+        }
+
+        public void sort() { 
+            
+            int temp = 0;
+
+            Stopwatch MyTimer = new Stopwatch();
+            MyTimer.Start();
+
+            for (int write = 0; write < myList.Count; write++)
+            {
+                for (int sort = 0; sort < myList.Count - 1; sort++)
+                {
+                    if (myList[sort] > myList[sort + 1])
+                    {
+                        temp = myList[sort + 1];
+                        myList[sort + 1] = myList[sort];
+                        myList[sort] = temp;
+                    }
+                }
+            }
+
+            MyTimer.Stop();
+            textBox2.Text = MyTimer.Elapsed.ToString();
+
+            foreach (int var in myList)
+            {
+                textBox6.Text += System.Environment.NewLine + var.ToString();
+            }
+        }
+
+        public void count() {
+
+            Stopwatch MyTimer = new Stopwatch();
+
+            MyTimer.Start();
+
+            var groups = myList.GroupBy(v => v);
+            foreach (var group in groups)
+            {      
+                var index = this.dataGridView1.Rows.Add();
+                dataGridView1.Rows[index].Cells[0].Value = group.Key;
+                dataGridView1.Rows[index].Cells[1].Value = group.Count();       
+            }
+            textBox3.Text = MyTimer.Elapsed.ToString();        
+
+            MyTimer.Stop();
+        }
+
+        private void button1_scan_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                generate();
+            }
+            catch (Exception ex)
+            {
+                msgBox(ex);
+            }
+        }
+
+        private void button2_sort_Click(object sender, EventArgs e)
+        {
+             try
+                {
+                    sort();
+                }
+                catch (Exception ex)
+                {
+                    msgBox(ex);
+                }
+        }
+
+        private void button3_result_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                count();
+            }
+            catch (Exception ex)
+            {
+                msgBox(ex);
+            }
+        }
+
+        private void msgBox(Exception ex)
+        {
+            var dialogTypeName = "System.Windows.Forms.PropertyGridInternal.GridErrorDlg";
+            var dialogType = typeof(Form).Assembly.GetType(dialogTypeName);
+            var dialog = (Form)Activator.CreateInstance(dialogType, new PropertyGrid());
+
+            dialog.Text = "Error Box";
+            dialogType.GetProperty("Details").SetValue(dialog, ex.ToString(), null);
+            dialogType.GetProperty("Message").SetValue(dialog, "An exception has been thrown !", null);
+
+            var result = dialog.ShowDialog();
+        }
+    }
+}
